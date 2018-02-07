997542854cc7cf2e8bdf2cb945ffa27e54285cac
Mathieu314 <mathieu.moneyron44@gmail.com>
2018-02-07 11:31:26 +0100

    Update IP scheme and sinus table

--- source/sin.vhd
+++ source/sin.vhd
@@ -11,7 +11,73 @@ end entity table;
 architecture behavioral of table_sin is
 
 begin
-	clock: process(CLK)
+	clock: process(ADRESSE)
 	begin
-		if rising_edge(CLK) then
-			
+		case ADDRESSE is
+			when "000000" => SIN <= "000000";
+			when "000001" => SIN <= "000001";
+			when "000010" => SIN <= "000011";
+			when "000011" => SIN <= "000100";
+			when "000100" => SIN <= "000110";
+			when "000101" => SIN <= "000111";
+			when "000110" => SIN <= "001000";
+			when "000111" => SIN <= "001010";
+			when "001000" => SIN <= "001011";
+			when "001001" => SIN <= "001100";
+			when "001010" => SIN <= "001101";
+			when "001011" => SIN <= "001101";
+			when "001100" => SIN <= "001110";
+			when "001101" => SIN <= "001111";
+			when "001110" => SIN <= "001111";
+			when "001111" => SIN <= "001111";
+			when "010000" => SIN <= "001111";
+			when "010001" => SIN <= "001111";
+			when "010010" => SIN <= "001111";
+			when "010011" => SIN <= "001111";
+			when "010100" => SIN <= "001110";
+			when "010101" => SIN <= "001101";
+			when "010110" => SIN <= "001101";
+			when "010111" => SIN <= "001100";
+			when "011000" => SIN <= "001011";
+			when "011001" => SIN <= "001010";
+			when "011010" => SIN <= "001000";
+			when "011011" => SIN <= "000111";
+			when "011100" => SIN <= "000110";
+			when "011101" => SIN <= "000100";
+			when "011110" => SIN <= "000011";
+			when "011111" => SIN <= "000001";
+			when "100000" => SIN <= "000000";
+			when "100001" => SIN <= "100001";
+			when "100010" => SIN <= "100011";
+			when "100011" => SIN <= "100100";
+			when "100100" => SIN <= "100110";
+			when "100101" => SIN <= "100111";
+			when "100110" => SIN <= "101000";
+			when "100111" => SIN <= "101010";
+			when "101000" => SIN <= "101011";
+			when "101001" => SIN <= "101100";
+			when "101010" => SIN <= "101101";
+			when "101011" => SIN <= "101101";
+			when "101100" => SIN <= "101110";
+			when "101101" => SIN <= "101111";
+			when "101110" => SIN <= "101111";
+			when "101111" => SIN <= "101111";
+			when "110000" => SIN <= "101111";
+			when "110001" => SIN <= "101111";
+			when "110010" => SIN <= "101111";
+			when "110011" => SIN <= "101111";
+			when "110100" => SIN <= "101110";
+			when "110101" => SIN <= "101101";
+			when "110110" => SIN <= "101101";
+			when "110111" => SIN <= "101100";
+			when "111000" => SIN <= "101011";
+			when "111001" => SIN <= "101010";
+			when "111010" => SIN <= "101000";
+			when "111011" => SIN <= "100111";
+			when "111100" => SIN <= "100110";
+			when "111101" => SIN <= "100100";
+			when "111110" => SIN <= "100011";
+			when "111111" => SIN <= "100001";
+		end case;
+	end process clock;
+end architecture behavioral;
--- table_sin.c
+++ table_sin.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 
 int main() {
-	int i;
+	int i, j, k;
 	int x;
+	char iInBin[7] = {'0'};
+	char xInBin[7] = {'0'};
 	for (i = 0 ; i < 64 ; i++) {
 		x  = lrint(31 * sinf(2 * 3.14 * i / 64));
-		printf("%d\n", x);
+		for (j = 6 ; j >= 0 ; j--) {
+			k = i >> j; 
+			iInBin[5 - j] = (k & 1) + '0';
+		}
+		iInBin[6] = '\0';
+
+		if (x < 0) {
+			xInBin[0] = '1';
+		} else {
+			xInBin[0] = '0';
+		}
+		for (j = 5 ; j > 0 ; j--) {
+			k = abs(x) >> j; 
+			xInBin[5 - j + 1] = (k & 1) + '0';
+		}
+		xInBin[6] = '\0';
+		printf("\t\t\t\twhen \"%s\" => SIN <= \"%s\";\n", iInBin, xInBin);
 	}
 	return 0;
 }
